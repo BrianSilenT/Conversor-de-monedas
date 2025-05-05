de38758c7176b03fd2d4ef6acd7b69dd730cf01a
brian <brianpls2002@gmail.com>
2025-05-04 20:04:15 -0600

    se crean y enlazan repositorios

---
+++ src/Principal/Principal.java
@@ -0,0 +1,11 @@
+package Principal;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        var Url = ("https://v6.exchangerate-api.com/v6/c3f53235016ae4afdae7a201/latest/USD");
+
+
+        
+    }
+}
